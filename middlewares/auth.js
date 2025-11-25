4db01dc8a8a9d5ada3970c374f5a3460788c650c
Marsonn07 <rafaelmarson.rafa@gmail.com>
2025-11-25 16:39:28 -0300

    Create auth.js

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const auth = (req, res, next) => {
+    
+    const token = req.headers.authorization;
+    console.log(token)
+
+    if(token){
+        return res.status(401).json({ message: ' Acesso negado'})
+    }
+    try {
+        const decoded = jwt.verify(token.replace('Bearer', ''), JWT_SECRET)
+        console.log(decoded)
+    }
+    catch (err){
+        return res.status(401).json({ message: ' Token inválido'})
+    }
+
+    next();
+}
+
+export default auth;
